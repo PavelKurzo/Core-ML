19bd70904431308fc80fba95d7c437bcfd5b9ba9
Pavel <pavel.kurzo@bereyziat.dev>
2022-11-24 15:39:39 +0100

    Core ML Demo application

---
+++ CoreMlProject/CoreMlProject/CoreMlProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreMlProjectApp.swift
+//  CoreMlProject
+//
+//  Created by Павел Курзо on 24/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct CoreMlProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
